196410d4f47cca0448b30c8781193b06c8bc12c1
Victor Nguyen <ngmthaq.20@gmail.com>
2025-12-18 23:33:38 +0700

    feat: init bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './styles/custom-bootstrap.scss';
+import './styles/index.scss';
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import './index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,11 @@ export default defineConfig({
       },
     }),
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ['import', 'color-functions', 'global-builtin', 'if-function'],
+      },
+    },
+  },
 });
